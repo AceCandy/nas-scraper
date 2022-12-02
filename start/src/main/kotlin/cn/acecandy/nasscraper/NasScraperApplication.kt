304793412ab71b0b1385b3b71607c774ce78bcc7
tangningzhu <ningzhu.tang@tripalink.com>
2022-12-02 15:26:19 +0800

    fisrt commit

---
+++ start/src/main/kotlin/cn/acecandy/nasscraper/NasScraperApplication.kt
@@ -0,0 +1,11 @@
+package cn.acecandy.nasscraper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NasScraperApplication
+
+fun main(args: Array<String>) {
+    runApplication<NasScraperApplication>(*args)
+}
